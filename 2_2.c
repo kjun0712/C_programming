efe83eac6e3bcd1206c50c81dc1584d5e3dd0f75
kjun0712 <kjun0712@gmail.com>
2024-07-02 20:23:16 +0900

    문제2-2

---
+++ 2_2.c
@@ -0,0 +1,18 @@
+/*
+//문제 1
+#include<stdio.h>
+
+int main(){
+    printf("제 이름은 홍길동입니다. \n");
+    printf("제 나이는 %d살이고요. \n",20);
+    printf("제가 사는 곳의 번지수는 %d-%d입니다.",123,456);
+}
+*/
+
+//문제 2
+#include<stdio.h>
+
+int main(){
+    printf("%dx%d=%d \n",4,5,20);
+    printf("%dx%d=%d",7,9,63);
+}
