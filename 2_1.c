7adb753aa687654f923842eef5209a86830fb4f9
kjun0712 <kjun0712@gmail.com>
2024-07-02 20:08:17 +0900

    문제02-1

---
+++ 2_1.c
@@ -0,0 +1,15 @@
+//문제 1
+#include<stdio.h>
+
+int main(){
+    printf("홍길동 \n홍 길 동 \n홍  길  동");
+}
+
+//문제2
+#include<stdio.h>
+
+int main(){
+    printf("이름 \n");
+    printf("주소 \n");
+    printf("010-7533-0936");
+}
