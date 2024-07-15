52d3d838244ce55f2682ddd5a4c7a6b1852e3471
kjun0712 <kjun0712@gmail.com>
2024-07-15 22:00:11 +0900

    문제07-3

--- 7-3.c
+++ 7-3.c
@@ -13,19 +13,37 @@ int main(){
     printf("%d",result);
 }
 */
-//문제 2  해결못함
+//문제 2
 #include<stdio.h>
 
 int main(){
     int a=0;
     int result=0;
-
+    int c=0;
     do{
-        do{
-            result+=a;
-        } while (a/2==0);
-        a++;
-    } while (a>=100);
+        result+=a;
+        a+=2;
+    } while (a<=100);
     
     printf("%d", result);
 }
+/*
+//문제3
+#include<stdio.h>
+
+int main(){
+    int a=2;
+    int b=0;
+    
+    do
+    {
+        b=1;
+        do
+        {
+            printf("%d\n",a*b);
+            b++;
+        } while (b<10);
+        a++;
+    } while (a<10);
+}
+*/
